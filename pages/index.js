d37b41dae161c2617fdeb25fd5bc178ab3dfe0d1
imbhargav5 <imbhargav5@gmail.com>
2021-01-03 18:26:54 +0530

    Init

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+function Home(){
+    return <div>
+        <h1> Delightful React </h1>
+        <img src="https://assets.delightful-react.com/scenery.jpg"/>
+        <p>The best way to learn React!</p>
+        <a href="https://google.com">Google</a>        
+        <br/>
+        <button>Click Me!</button>
+      </div>
+}
+
+export default Home;
